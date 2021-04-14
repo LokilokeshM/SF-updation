088b8c9a213c570583c0a9bf3716eab2a953b66a
LokilokeshM <78999166+LokilokeshM@users.noreply.github.com>
2021-04-14 19:44:08 +0530

    FrontEnd And BackEnd

---
+++ BackEnd/SocietyFinancialManagement/src/main/java/com/java/srm/SocietyFinancialManagement/SocietyFinancialManagementApplication.java
@@ -0,0 +1,14 @@
+package com.java.srm.SocietyFinancialManagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocietyFinancialManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocietyFinancialManagementApplication.class, args);
+		System.out.println("Success");
+	}
+
+}
---
+++ BackEnd/SocietyFinancialManagement/src/test/java/com/java/srm/SocietyFinancialManagement/SocietyFinancialManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java.srm.SocietyFinancialManagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocietyFinancialManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
